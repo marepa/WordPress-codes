d3056bb7164ea045317725a01a0bf1d336f1a084
marepa <pamare273@gmail.com>
2022-08-14 16:11:47 +0200

    Update load-footer-webtero.php

--- load-footer-webtero.php
+++ load-footer-webtero.php
@@ -1,15 +1,16 @@
-add_filter( 'sikmo/block/maybe_located', function( $maybe_located, $args ) {
-	global $fce_front;
+<?php
+	add_filter( 'sikmo/block/maybe_located', function( $maybe_located, $args ) {
+		global $fce_front;
 
-	if( self::$located ) {
-		self::$located = false;
+		if( self::$located ) {
+			self::$located = false;
 
-		// load anything at beginning of footer	
-	}
+			// load anything at beginning of footer	
+		}
 
-	if( str_contains( $maybe_located, 'footer.php' ) ) {
-		self::$located = true;
-	}
+		if( str_contains( $maybe_located, 'footer.php' ) ) {
+			self::$located = true;
+		}
 
-	return $maybe_located;
-}, 20, 2);
+		return $maybe_located;
+	}, 20, 2);
